f2cb8c663a6c84e4795f66592bff19b1dd2d5db8
Pedro Albuquerque <pedro.melo.albuquerque@gmail.com>
2020-03-28 19:02:28 -0400

    Links e dados para a análise espacial

---
+++ Espacial/DF/pop_setor.R
@@ -0,0 +1,5 @@
+#População por setor censitario
+#ftp://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/DF_20171016.zip
+
+#Mlahas setor censitario
+#ftp://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_de_setores_censitarios__divisoes_intramunicipais/censo_2010/setores_censitarios_shp/df/
---
+++ Espacial/Municipal/pop_municipal.R
@@ -0,0 +1,9 @@
+library(devtools)
+devtools::install_github("cran/ipeaData")
+library(ipeaData)
+
+#População por município.
+#http://ipeadata.gov.br/beta3/#/lista-de-serie?ascOrder=desc&base=regional&busca=&columnOrdering=SERATUALIZACAO&fonte=&skip=0&tema=Popula%C3%A7%C3%A3o
+
+#Mlahas municipais
+#https://mapas.ibge.gov.br/bases-e-referenciais/bases-cartograficas/malhas-digitais
